43897f046bf1c4b89663128bb452a32aa385097c
unknown <mehedi15-5815@diu.edu.bd>
2022-04-04 13:43:02 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Greed from './Components/Greet';
+import Welcome from './Components/welcome';
 
 function App() {
   return (
     <div className="App">
       <Greed />
+      <Welcome />
     </div>
   );
 }
