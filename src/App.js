2b7526010fbbafa474f89538b3a0c8ca5cf5a20b
unknown <mehedi15-5815@diu.edu.bd>
2022-04-04 13:04:10 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Greed from './Components/Greet';
+
+function App() {
+  return (
+    <div className="App">
+      <Greed />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greed()
+// {
+//     return <h1>Hello Lemon</h1>
+// }
+const Greed = () => <h1>Hello Lemon</h1>
+export default Greed
