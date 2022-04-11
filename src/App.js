e08e3a42ac81d944efc2fd40799b3727e0f3ac6c
unknown <mehedi15-5815@diu.edu.bd>
2022-04-11 11:40:43 +0600

    commit state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Greed from './Components/Greet';
 import Welcome from './Components/welcome';
 import Message from './Components/Message';
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Greed name= "Lemon" heroName= "Thor">
+      <Counter />
+      {/* <Greed name= "Lemon" heroName= "Thor">
         <p>This is child</p>
       </Greed>
       <Greed name= "Mim" heroName= "Iron Man" >
@@ -17,7 +18,7 @@ function App() {
       <Welcome name= "Lemon" heroName= "Thor" />
       <Welcome name= "Mim" heroName= "Iron Man" />
       <Welcome name= "Mehedi" heroName= "Spider Man" />
-      <Message />
+      <Message /> */}
     </div>
   );
 }
