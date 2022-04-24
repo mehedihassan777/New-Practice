adf6e54ea18e6a6959df81f0ec5f798e50e7fe4e
unknown <mehedi15-5815@diu.edu.bd>
2022-04-24 12:52:07 +0600

    connect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-=======
 import './App.css';
 import Greed from './Components/Greet';
 import Welcome from './Components/welcome';
 import Message from './Components/Message';
 import Counter from './Components/Counter';
->>>>>>> 9e3409b5b12870e9beb7698566b095cf528138c6
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Counter />
       {/* <Greed name= "Lemon" heroName= "Thor">
         <p>This is child</p>
@@ -40,7 +19,6 @@ function App() {
       <Welcome name= "Mim" heroName= "Iron Man" />
       <Welcome name= "Mehedi" heroName= "Spider Man" />
       <Message /> */}
->>>>>>> 9e3409b5b12870e9beb7698566b095cf528138c6
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom/client';
-=======
-import ReactDOM from 'react-dom';
->>>>>>> 9e3409b5b12870e9beb7698566b095cf528138c6
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-<<<<<<< HEAD
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-=======
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
->>>>>>> 9e3409b5b12870e9beb7698566b095cf528138c6
 );
 
 // If you want to start measuring performance in your app, pass a function
